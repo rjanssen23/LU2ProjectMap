e04c7a644c7a39226aa066f0c42cbd98c1397458
rjanssen23 <r.janssen23@student.avans.nl>
2025-04-08 12:02:37 +0200

    Project aangemaakt

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Unity.VisualScripting;
+
+public class LoginManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
